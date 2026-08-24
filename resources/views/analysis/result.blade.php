a3bec75662df0fa04ebf96773a4e80c74da6cec6
irhamkaraman <irhamkaraman@gmail.com>
2026-08-24 19:57:38 +0700

    fix: Remove residual HTML artifacts from previous restructuring

--- resources/views/analysis/result.blade.php
+++ resources/views/analysis/result.blade.php
@@ -749,17 +749,8 @@
                                                     </button>
                                                 </div>
                                             @endif
-                                        </div>4 24">
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
-                                                        </svg>
-                                                        <span class="lang-id">Relasi Kuasa</span>
-                                                        <span class="lang-en">Power Relation</span>
-                                                        <span class="lang-zh">权力关系</span>
-                                                    </button>
-                                                @endif
-                                            </div>
                                         </div>
+                                    </div>
                                 </div>
 
                                 {{-- Agent Insight Banner --}}
